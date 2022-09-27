ba76c7640e7dbe2d53cadb3ff2d4fbdb2ae67e08
abhishekgarfield <abhishek2759@gmail.com>
2022-09-27 20:16:59 +0530

    meessenger

--- src/App.js
+++ src/App.js
@@ -1,7 +1,63 @@
-import "./App.css";
+import React, { useEffect, useState } from "react";
+import { StreamChat } from "stream-chat";
+import {
+  Chat,
+  Channel,
+  ChannelHeader,
+  ChannelList,
+  MessageList,
+  MessageInput,
+  Thread,
+  Window,
+} from "stream-chat-react";
+import "@stream-io/stream-chat-css/dist/css/index.css";
 
-function App() {
-  return <div className="App"></div>;
-}
+const filters = { type: "messaging" };
+const options = { state: true, presence: true, limit: 10 };
+const sort = { last_message_at: -1 };
+
+const App = () => {
+  const [client, setClient] = useState(null);
+
+  useEffect(() => {
+    const newClient = new StreamChat("7uk62d5vhgpe");
+
+    const handleConnectionChange = ({ online = false }) => {
+      if (!online) return console.log("connection lost");
+      setClient(newClient);
+    };
+
+    newClient.on("connection.changed", handleConnectionChange);
+
+    newClient.connectUser(
+      {
+        id: "dave-matthews",
+        name: "Dave Matthews",
+      },
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiZGF2ZS1tYXR0aGV3cyJ9.4Qqvr1CQIMKjanBnyCBtCiHHBdLx1as4hBNlOGwkiKY"
+    );
+
+    return () => {
+      newClient.off("connection.changed", handleConnectionChange);
+      newClient.disconnectUser().then(() => console.log("connection closed"));
+    };
+  }, []);
+
+  if (!client) return null;
+
+  return (
+    <Chat client={client}>
+      <ChannelList filters={filters} sort={sort} options={options} />
+      <Channel>
+        <Window>
+          <ChannelHeader />
+          <MessageList />
+          <MessageInput />
+        </Window>
+        <Thread />
+      </Channel>
+    </Chat>
+  );
+};
 
 export default App;
