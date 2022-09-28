8cad10cd59cccfccdd65c22b9dae38875fb0f28a
abhishekgarfield <abhishek2759@gmail.com>
2022-09-28 09:58:02 +0530

    dark ode

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { StreamChat } from "stream-chat";
 import {
   Chat,
@@ -18,9 +18,13 @@ const sort = { last_message_at: -1 };
 
 const App = () => {
   const [client, setClient] = useState(null);
+  const[channel,setchannel]=useState(null);
+  const modecheck=useRef(window.matchMedia("(prefers-color-scheme:dark)").matches);
+  console.log(modecheck.current)
 
   useEffect(() => {
-    const newClient = new StreamChat("7uk62d5vhgpe");
+    const newClient = new StreamChat("46gb46cpf2hp");
+    const newClient2 = new StreamChat("46gb46cpf2hp");
 
     const handleConnectionChange = ({ online = false }) => {
       if (!online) return console.log("connection lost");
@@ -28,14 +32,27 @@ const App = () => {
     };
 
     newClient.on("connection.changed", handleConnectionChange);
-
+    newClient2.on("connection.changed", handleConnectionChange);
+   
     newClient.connectUser(
       {
         id: "dave-matthews",
         name: "Dave Matthews",
       },
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiZGF2ZS1tYXR0aGV3cyJ9.4Qqvr1CQIMKjanBnyCBtCiHHBdLx1as4hBNlOGwkiKY"
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiZGF2ZS1tYXR0aGV3cyJ9.qv9QD7YIW_nWlSPBn7kQRBG01zVUqeUQeC6-ZWGX5PM"
     );
+    newClient2.connectUser(
+      {
+        id: "garfield",
+        name: "garfield",
+      },
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiZ2FyZmllbGQifQ.D7JPV4jczTPBoWnKdJ5qZyjRqppTSEufevhUcuC8e_M"
+    );
+    const channel = newClient2.channel('messaging', {
+      name:"demo",
+      members: ['dave-matthews','garfield'],
+  })
+    setchannel(channel);
 
     return () => {
       newClient.off("connection.changed", handleConnectionChange);
@@ -46,9 +63,9 @@ const App = () => {
   if (!client) return null;
 
   return (
-    <Chat client={client}>
+    <Chat client={client} darkMode={modecheck.current}>
       <ChannelList filters={filters} sort={sort} options={options} />
-      <Channel>
+      <Channel channel={channel}  >
         <Window>
           <ChannelHeader />
           <MessageList />
