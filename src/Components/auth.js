6805957fb3352b751448bd40e91dd41fa247c7e7
abhishekgarfield <abhishek2759@gmail.com>
2022-09-28 14:20:08 +0530

    chanes

--- src/Components/auth.js
+++ src/Components/auth.js
@@ -1,19 +1,22 @@
 import { useState } from "react";
 
 const Auth = () => {
-  const [user,setUser] = useState({
+  const [islogin, setislogin] = useState(true);
+  const [user, setUser] = useState({
     email: "",
     Password: "",
     confirmPassword: "",
   });
   const handleChange = (e) => {
-    const value=e.target.value;
-    const name=e.target.name;
-    
-  setUser({...user,[name]:value});
-  console.log(user);
+    const value = e.target.value;
+    const name = e.target.name;
 
+    setUser({ ...user, [name]: value });
+    console.log(user);
   };
+  const handleSubmit=()=>{
+    
+  }
   return (
     <div className="main-container">
       <div className="inside-container">
@@ -23,24 +26,55 @@ const Auth = () => {
           name="email"
           id="email"
           value={user.email}
-          onInput={(e)=>{handleChange(e)}}
+          onInput={(e) => {
+            handleChange(e);
+          }}
         />
-         <label htmlFor="email">Password</label>
-         <input
+        <label htmlFor="email">Password</label>
+        <input
           type="Password"
           name="Password"
           id="Password"
           value={user.Password}
-          onChange={(e)=>{handleChange(e)}}
+          onChange={(e) => {
+            handleChange(e);
+          }}
         />
-         <label htmlFor="email">Confirm Password</label>
-         <input
-          type="Password"
-          name="confirmPassword"
-          id="confirmPassword"
-          value={user.confirmPassword}
-          onChange={(e)=>{handleChange(e)}}
-        />
+        {!islogin && (
+          <>
+            {" "}
+            <label htmlFor="email">Confirm Password</label>
+            <input
+              type="Password"
+              name="confirmPassword"
+              id="confirmPassword"
+              value={user.confirmPassword}
+              onChange={(e) => {
+                handleChange(e);
+              }}
+            />
+          </>
+        )}
+        <div className="set-model">
+          <input
+            type="button"
+            value="Login"
+            onClick={() => {
+              setislogin(true);
+            }}
+          />
+          <input
+            type="button"
+            value="Sign up"
+            onClick={() => {
+                handleSubmit();
+              setislogin(false);
+            }}
+          />
+        </div>
+        <div className="go">
+            Go!
+        </div>
       </div>
     </div>
   );
